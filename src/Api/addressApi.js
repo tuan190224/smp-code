41bb041e68bb3a738b30c328c7a73dbed94667f3
tuan190224 <93728539+tuan190224@users.noreply.github.com>
2022-05-30 16:45:17 +0700

    add prettierrc

--- src/Api/addressApi.js
+++ src/Api/addressApi.js
@@ -1,25 +1,25 @@
-import axiosClient from "./axiosClient";
+import axiosClient from './axiosClient';
 // api/productApi.js
 const addressApi = {
-  getAll: (params) => {
-    const url = "/?depth=3";
-    return axiosClient.get(url, {
-      params,
-      baseURL: process.env.REACT_APP_API_ADDRESS,
-    });
-  },
-  //   put: (id, params) => {
-  //     const url = `/products/${id}`;
-  //     console.log(params);
+    getAll: (params) => {
+        const url = '/?depth=3';
+        return axiosClient.get(url, {
+            params,
+            baseURL: process.env.REACT_APP_API_ADDRESS,
+        });
+    },
+    //   put: (id, params) => {
+    //     const url = `/products/${id}`;
+    //     console.log(params);
 
-  //     return axiosClient.put(url, params);
-  //   },
-  //   post: (params) => {
-  //     const url = "/products";
-  //     console.log(params);
+    //     return axiosClient.put(url, params);
+    //   },
+    //   post: (params) => {
+    //     const url = "/products";
+    //     console.log(params);
 
-  //     return axiosClient.post(url, params);
-  //   },
+    //     return axiosClient.post(url, params);
+    //   },
 };
 
 export default addressApi;
--- src/Api/loginApi.js
+++ src/Api/loginApi.js
@@ -1,19 +1,19 @@
-import axiosClient from "./axiosClient";
+import axiosClient from './axiosClient';
 
 const loginApi = {
-  post: (params) => {
-    const url = "/users";
-    return axiosClient.post(url, {
-      baseURL: process.env.REACT_APP_API_ULR_2,
-      params,
-    });
-  },
-  get: (id, params) => {
-    const url = `/users/${id}`;
-    return axiosClient.get(url, {
-      baseURL: process.env.REACT_APP_API_ULR_2,
-      params,
-    });
-  },
+    post: (params) => {
+        const url = '/users';
+        return axiosClient.post(url, {
+            baseURL: process.env.REACT_APP_API_ULR_2,
+            params,
+        });
+    },
+    get: (id, params) => {
+        const url = `/users/${id}`;
+        return axiosClient.get(url, {
+            baseURL: process.env.REACT_APP_API_ULR_2,
+            params,
+        });
+    },
 };
 export default loginApi;
